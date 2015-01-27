88554d2f1a53fec0b7f8ba3b901e98a3d09ba7d5
dhirajagarwal <dhirajagarwal@live.com>
2015-01-27 11:49:03 +0530

    search moved to header

--- www/js/app.js
+++ www/js/app.js
@@ -1,69 +1,62 @@
-// Ionic Starter App
+/**
+ * Created by dhirajagarwal on 25/01/15.
+ */
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
 
-angular.module('doctorApp', ['ionic', 'doctorApp.services', 'doctorApp.controllers'])
+angular.module('doctorApp.controllers', [])
 
-    .run(function ($ionicPlatform) {
-        $ionicPlatform.ready(function () {
-            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-            // for form inputs)
-            if (window.cordova && window.cordova.plugins.Keyboard) {
-                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-            }
-            if (window.StatusBar) {
-                // org.apache.cordova.statusbar required
-                StatusBar.styleDefault();
-            }
-            ionic.Platform.isFullScreen = false;
-        });
+    .controller('DoctorCtrl', function ($scope, Data) {
+        $scope.allDocs = Data.allData();
+
+        $scope.search = {};
+        $scope.search.searchIcon = false;
+        $scope.search.searchText = "";
+
+        $scope.searchClicked = function(){
+            $scope.search.searchText = "";
+            $scope.search.searchIcon = !$scope.search.searchIcon;
+        }
+
     })
 
-    .config(function ($stateProvider, $urlRouterProvider) {
+    .controller('DoctorDetailsCtrl', function ($scope, $stateParams, Data){
+        $scope.doctorInde = $stateParams.doctorIndex;
+        $scope.doctorDetails = Data.getDoc($stateParams.doctorIndex);
+    })
 
-        // Ionic uses AngularUI Router which uses the concept of states
-        // Learn more here: https://github.com/angular-ui/ui-router
-        // Set up the various states which the app can be in.
-        // Each state's controller can be found in controllers.js
-        $stateProvider
+    .controller('PatientDetailsCtrl', function ($scope, $stateParams, Data){
+        $scope.patientDetails = Data.getDoc($stateParams.doctorInd).patients[$stateParams.patientIndex]
+    })
 
-            // setup an abstract state for the tabs directive
-            .state('doctors', {
-                url: "/",
-                templateUrl: "templates/doctors.html",
-                controller: 'DoctorCtrl'
-            })
+    .controller('AddDoctorCtrl', function ($scope, Data, $state) {
+        $scope.doctor = {};
+        $scope.addDoctor = function () {
+            var doc = {
+                name: $scope.doctor.firstname + ' ' + $scope.doctor.lastname,
+                address: '',
+                phone: $scope.doctor.tel,
+                amount: 0,
+                due: 0,
+                edit: false,
+                patients:[]
+            };
+            Data.addDoc(doc);
+            $state.go('doctors');
+        }
+    })
 
-            .state('doctor-details', {
-                url: '/doctor-patients/:doctorIndex',
-                templateUrl: 'templates/doctor-patients.html',
-                controller: 'DoctorDetailsCtrl'
-            })
-
-            .state('patient-details', {
-                url: '/patientDetails/:patientIndex/:doctorInd',
-                templateUrl: 'templates/patient-details.html',
-                controller: 'PatientDetailsCtrl'
-            })
-
-            .state('add-patient', {
-                url: '/addPatient/:doctorInd',
-                templateUrl: 'templates/addPatient.html',
-                controller: 'AddPatientCtrl'
-            })
-
-            .state('add-doctor', {
-                url: '/doctorAdd',
-                templateUrl: 'templates/addDoctor.html',
-                controller: 'AddDoctorCtrl'
-            })
-
-
-        // if none of the above states are matched, use this as the fallback
-        $urlRouterProvider.otherwise('/');
-
+    .controller('AddPatientCtrl', function ($scope, Data, $state, $stateParams) {
+        $scope.doctorInde = $stateParams.doctorInd;
+        $scope.patient = {};
+        $scope.addPatient = function () {
+            var patient = {
+                name: $scope.patient.firstname + ' ' + $scope.patient.lastname,
+                address: '',
+                phone: $scope.patient.tel,
+                amount: 0,
+                edit: false
+            };
+            Data.addPat(patient, $stateParams.doctorInd);
+            $state.go('doctor-details', {doctorIndex : $stateParams.doctorInd});
+        }
     });
